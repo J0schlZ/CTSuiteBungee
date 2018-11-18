554f40d531d8efdde8311574843cd15e9d0e735d
J0schlZ <j0schlz@craft-together.de>
2018-11-18 17:49:04 +0100

    First commit

---
+++ CTSuiteBungee/src/main/java/de/crafttogether/ctsuite/bungee/CTSuite.java
@@ -0,0 +1,125 @@
+package de.crafttogether.ctsuite.bungee;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.text.SimpleDateFormat;
+
+import com.google.common.io.ByteStreams;
+import com.zaxxer.hikari.HikariDataSource;
+
+import de.crafttogether.ctsuite.bungee.events.PlayerJoinListener;
+import de.crafttogether.ctsuite.bungee.handlers.MessageHandler;
+import de.crafttogether.ctsuite.bungee.handlers.PermissionHandler;
+import net.md_5.bungee.api.plugin.Plugin;
+import net.md_5.bungee.config.Configuration;
+import net.md_5.bungee.config.ConfigurationProvider;
+import net.md_5.bungee.config.YamlConfiguration;
+
+public class CTSuite extends Plugin {
+	
+    private static CTSuite instance;
+    private HikariDataSource hikari;
+    private Configuration config;
+    private String tablePrefix;
+    private SimpleDateFormat dateFormat;
+    
+    private PermissionHandler permissionHandler;
+    private MessageHandler messageHandler;
+
+    public void onEnable() {
+        instance = this;
+        
+        if (!getDataFolder().exists()) {
+            getDataFolder().mkdir();
+        }
+        File configFile = new File(getDataFolder(), "config.yml");
+        File messagesFile = new File(getDataFolder(), "messages.yml");
+        File announcementsFile = new File(getDataFolder(), "announcements.yml");
+        try {
+            if (!configFile.exists()) {
+                configFile.createNewFile();
+                InputStream is = getResourceAsStream("config.yml");
+                OutputStream os = new FileOutputStream(configFile);
+                ByteStreams.copy(is, os);
+            }
+            if (!messagesFile.exists()) {
+                messagesFile.createNewFile();
+                InputStream is = getResourceAsStream("messages.yml");
+                OutputStream os = new FileOutputStream(messagesFile);
+                ByteStreams.copy(is, os);
+            }
+            if (!announcementsFile.exists()) {
+                announcementsFile.createNewFile();
+                InputStream is = getResourceAsStream("announcements.yml");
+                OutputStream os = new FileOutputStream(announcementsFile);
+                ByteStreams.copy(is, os);
+            }
+        } catch (IOException e) {
+            throw new RuntimeException("Unable to create file", e);
+        }
+        try {
+            config = ConfigurationProvider.getProvider(YamlConfiguration.class).load(new InputStreamReader(new FileInputStream(configFile), "UTF8"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        this.hikari = new HikariDataSource();
+        this.hikari.setDataSourceClassName("com.mysql.jdbc.jdbc2.optional.MysqlDataSource");
+        this.hikari.addDataSourceProperty("serverName", this.config.get("MySQL.host"));
+        this.hikari.addDataSourceProperty("port", this.config.get("MySQL.port"));
+        this.hikari.addDataSourceProperty("databaseName", this.config.get("MySQL.database"));
+        this.hikari.addDataSourceProperty("user", this.config.get("MySQL.user"));
+        this.hikari.addDataSourceProperty("password", this.config.get("MySQL.password"));
+        
+        tablePrefix = this.config.getString("MySQL.prefix");
+        dateFormat = new SimpleDateFormat(config.getString("CTSuite.Messages.TimeFormat"));
+        
+        permissionHandler = new PermissionHandler(this);
+        messageHandler = new MessageHandler(this);
+
+        messageHandler.readMessagesFromFile();
+        permissionHandler.readAvailablePermissionsFromFile();
+        
+        getProxy().registerChannel("CTSuite");
+        getProxy().getPluginManager().registerListener(this, new PlayerJoinListener(this));
+    }
+
+    public void onDisable() {
+        if (this.hikari != null) {
+            this.hikari.close();
+        }
+    }
+
+    public Configuration getConfig() {
+        return config;
+    }
+    
+    public String getTablePrefix() {
+    	return tablePrefix;
+    }
+
+    public MessageHandler getMessageHandler() {
+        return messageHandler;
+    }
+
+    public PermissionHandler getPermissionHandler() {
+        return permissionHandler;
+    }
+    
+    public HikariDataSource getHikari() {
+        return this.hikari;
+    }
+    
+    public SimpleDateFormat getDateFormat() {
+        return dateFormat;
+    }
+    
+    public static CTSuite getInstance() {
+        return instance;
+    }
+}
---
+++ CTSuiteBungee/src/main/java/de/crafttogether/ctsuite/bungee/events/PlayerJoinListener.java
@@ -0,0 +1,71 @@
+package de.crafttogether.ctsuite.bungee.events;
+
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.event.*;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Date;
+
+import de.crafttogether.ctsuite.bungee.CTSuite;
+
+public class PlayerJoinListener implements Listener {
+    private CTSuite main;
+
+    public PlayerJoinListener(CTSuite main) {
+        this.main = main;
+    }
+
+    @EventHandler
+    public void onLogin(LoginEvent ev) {
+        ev.registerIntent(main);
+        try {
+        	String sql = "SELECT * FROM " + main.getTablePrefix() + "bans " + "WHERE player = '" + ev.getConnection().getUniqueId() + "' ORDER BY -(expiration IS NULL), expiration DESC LIMIT 1";
+            ResultSet rs = main.getHikari().getConnection().createStatement().executeQuery(sql);
+            
+            if (rs.next()) {
+                Date expiration = rs.getTimestamp("expiration");
+                
+                if (rs.wasNull() || expiration.after(new Date())) {
+                    ev.setCancelled(true);
+                    String reason;
+                    
+                    if (rs.wasNull())
+                        reason = main.getMessageHandler().getMessage("join.banned").replace("%reason%", rs.getString("reason"));
+                    else
+                        reason = main.getMessageHandler().getMessage("join.banneduntil").replace("%reason%", rs.getString("reason")).replace("%expiration%", main.getDateFormat().format(expiration));
+                    
+                    ev.setCancelReason(new TextComponent(reason));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        
+        /*
+        String ip = ev.getConnection().getAddress().getAddress().toString();
+        if (!ev.isCancelled() && main.getConfig().getInt("CTSuite.Security.MaxPlayersWithSameIP") > 0) {
+            if (main.getPlayerHandler().getIps().containsKey(ip) && main.getPlayerHandler().getIps().get(ip) >= main
+                    .getConfig().getInt("CTSuite.Security.MaxPlayersWithSameIP")) {
+                ev.setCancelled(true);
+                ev.setCancelReason(new TextComponent(main.getMessageHandler().getMessage("security.join.denied")));
+            }
+        }
+        */
+
+        /*
+        if (!ev.isCancelled()) {
+            main.getPlayerHandler().registerLogin(ev.getConnection());
+
+            if (main.getPlayerHandler().getIps().containsKey(ip))
+                main.getPlayerHandler().getIps().put(ip, main.getPlayerHandler().getIps().get(ip) + 1);
+            else
+                main.getPlayerHandler().getIps().put(ip, 1);
+        }
+        */
+        
+        ev.completeIntent(main);
+    }
+}
