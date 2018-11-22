f32fb05b470a62e4b997b77cd24df88107450351
J0schlZ <j0schlz@craft-together.de>
2018-11-23 00:18:52 +0100

    Ich glaub das reicht für Heute

--- CTSuiteBungee/src/main/java/de/crafttogether/ctsuite/bungee/events/PlayerLoginListener.java
+++ CTSuiteBungee/src/main/java/de/crafttogether/ctsuite/bungee/events/PlayerLoginListener.java
@@ -1,17 +1,16 @@
 package de.crafttogether.ctsuite.bungee.events;
 
-import net.md_5.bungee.api.chat.TextComponent;
-import net.md_5.bungee.api.event.*;
-import net.md_5.bungee.api.plugin.Listener;
-import net.md_5.bungee.event.EventHandler;
-
-import java.sql.Statement;
 import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.Date;
 
 import de.crafttogether.ctsuite.bungee.CTSuite;
+import net.md_5.bungee.api.chat.TextComponent;
+import net.md_5.bungee.api.event.LoginEvent;
+import net.md_5.bungee.api.plugin.Listener;
+import net.md_5.bungee.event.EventHandler;
 
 public class PlayerLoginListener implements Listener {
     private CTSuite main;
@@ -24,25 +23,22 @@ public class PlayerLoginListener implements Listener {
     public void onLogin(LoginEvent ev) {
         ev.registerIntent(main);
         
-        String sql;
-        ResultSet rs = null;
-        Statement statement = null;
-        Connection conn = null;
+        String uuid = ev.getConnection().getUniqueId().toString();
         
-        sql = 
-          "SELECT * FROM " + main.getTablePrefix() + "bans " +
-          "WHERE player = '" + ev.getConnection().getUniqueId() + "' " +
-          "ORDER BY -(expiration IS NULL), expiration DESC LIMIT 1";
+        ResultSet resultSet = null;
+        PreparedStatement statement = null;
+        Connection connection = null;
         
         try {
-        	conn = main.getConnection();
-        	statement = conn.createStatement();
-        	rs = statement.executeQuery(sql);
+        	connection = main.getConnection();
+			statement = connection.prepareStatement("SELECT * FROM " + main.getTablePrefix() + "bans WHERE uuid = ? ORDER BY -(expiration IS NULL), expiration DESC LIMIT 1");
+			statement.setString(1, uuid);
+			resultSet = statement.executeQuery();
         	
-            if (rs.next()) {
-                Date expiration = rs.getTimestamp("expiration");
+            if (resultSet.next()) {
+                Date expiration = resultSet.getTimestamp("expiration");
                 
-                if (rs.wasNull() || expiration.after(new Date())) {
+                if (resultSet.wasNull() || expiration.after(new Date())) {
                     ev.setCancelled(true);
                     String reason = "banned";
                     
@@ -57,16 +53,16 @@ public class PlayerLoginListener implements Listener {
         } catch (SQLException e) {
             e.printStackTrace();
         } finally {
-            if (rs != null) {
-                try { rs.close(); }
+            if (resultSet != null) {
+                try { resultSet.close(); }
                 catch (SQLException e) { e.printStackTrace(); }
             }
             if (statement != null) {
                 try { statement.close(); }
                 catch (SQLException e) { e.printStackTrace(); }
             }
-            if (conn != null) {
-                try { conn.close(); }
+            if (connection != null) {
+                try { connection.close(); }
                 catch (SQLException e) { e.printStackTrace(); }
             }
         }
