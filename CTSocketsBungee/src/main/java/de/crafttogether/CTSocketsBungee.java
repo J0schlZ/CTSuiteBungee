308f6cf692587d26c7a2bff546807bed2d8d72eb
J0schlZ <j0schlz@gmx.net>
2019-08-18 08:26:03 +0200

    Los gehts....

---
+++ CTSocketsBungee/src/main/java/de/crafttogether/CTSocketsBungee.java
@@ -0,0 +1,14 @@
+package de.crafttogether;
+
+import net.md_5.bungee.api.plugin.Plugin;
+
+public class CTSocketsBungee extends Plugin {
+	
+	public void onEnable() {
+		
+	}
+	
+	public void onDisable() {
+		
+	}
+}
